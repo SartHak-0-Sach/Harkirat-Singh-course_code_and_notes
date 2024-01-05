89fbced4adfc40ed0e5ccd4d4a0e321807533f41
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-05 14:18:37 +0530

    .

---
+++ Week-01/lec-3/code.js
@@ -0,0 +1,17 @@
+var a = 1;
+a=2;
+console.log(a);
+
+let b = 1;
+b=2;
+console.log(b);
+
+const c = 9;
+// c = 7;
+console.log(c);
+
+let firstName = "Sarthak";
+let age = 19;
+let isMarried = false;
+
+console.log("My name is " + firstName + " and my age is " + age);
