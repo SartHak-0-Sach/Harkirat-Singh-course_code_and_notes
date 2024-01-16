5b6cdb2805d9e292e81a696ac12c730228d4c593
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-16 12:00:44 +0530

    Update anagram.js

--- Week-01/01-js=assignments/easy/anagram.js
+++ Week-01/01-js=assignments/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length != str2.length)
+  return false;
 
+  const sortedString1 = str1.split('').sort().join('');
+  const sortedString2 = str2.split('').sort().join('');
+
+  for(let i = 0; i<str1.length; i++)
+  {
+    if(str1[i]!=str2[i])
+    return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
