50d1a181015e0a3f3ee2ee6483441accb392967b
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-02-01 09:48:55 +0530

    Update code.js

--- Week-02/lec-1/code.js
+++ Week-02/lec-1/code.js
@@ -1,121 +1,127 @@
-// var a = 1;
-// a = 2;
-// console.log(a);
+// understanding callback hell by using a scenario and writing normal callback and async await code for it
 
-// let b = 1;
-// b = 2;
-// console.log(b);
-
-// const c = 9;
-// // c = 7;
-// console.log(c);
-
-// let firstName = "Sarthak";
-// let age = 19;
-// let isMarried = false;
-
-// console.log("My name is " + firstName + " and my age is " + age);
-
-// if (isMarried) {
-//     console.log(firstName + "is married");
-// }
-// else {
-//     console.log(firstName + "not married")
-// }
-
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-// Program-0 Print sum of all numbers from 1 to n using for, while, do-while loops
-
-// const readline = require('readline');
-
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// var totalSum = (n) => {
-//     let ans = 0;
-//     for (let i = 1; i <= n; i++) {
-//         ans += i;
-//     }
-//     console.log(ans);
-// }
-
-// var totalSum2 = function (n) {
-//     let ans = 0;
-//     let i = 1;
-//     while (i <= n) {
-//         ans += i;
-//         i++;
-//     }
-//     console.log(ans);
-// }
-
-// var totalSum3 = (n) => {
-//     let ans = 0;
-//     let i = 1;
-//     do {
-//         ans += i;
-//         i++;
-//     } while (i <= n);
-//     console.log(ans);
-// }
-
-// rl.question('Enter a number: ', (answer) => {
-//     let n = parseInt(answer);
-//     totalSum(n);
-//     totalSum2(n);
-//     totalSum3(n);
-//     rl.close();
-// });
+// Write a function that says hello after one second, then hello twice after 2 seconds and then hello thrice 3 seconds after that
 
 
-// Program-1 Write a program that greets a person given their first and last name
+// standard callback code
+const SayingHello = () => console.log("Hello");
+function delayCallback(callback, count, duration) {
+    setTimeout(function () {
+        for (let i = 0; i < count; i++)
+            callback();
+    }, duration)
+}
 
 
+function delayDemo() {
+    let duration = 1000;
+    delayCallback(SayingHello, 1, duration);
+    duration += 2000;
+    delayCallback(SayingHello, 2, duration);
+    duration += 2000;
+    delayCallback(SayingHello, 3, duration);
+}
 
-// Program-2 Write a program that greets a person based on their gender.
-
-
-
-// Program-3 Write a program that counts from 0-1000 and prints all of these numbers.
-
-// for(let i = 0; i <= 1000; i++)
-// {
-//     console.log(i);
-// }
-
-// Program-4 Write a program that prints all even numbers in an array.
+delayDemo();
 
 
+// using promises
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    });
+}
 
-// Program-5 Write a program that can print the biggest number in an array.
+SayingHello = () => console.log("Hello");
+
+function printHelloAfterDelay(delayTime, times) {
+    return delay(delayTime)
+        .then(() => {
+            for (let i = 0; i < times; i++) {
+                SayingHello();
+            }
+        });
+}
+
+// Using async/await
+async function printHelloAsync() {
+    await delay(1000);
+    SayingHello();
+
+    await printHelloAfterDelay(2000, 2);
+
+    await printHelloAfterDelay(3000, 3);
+}
+
+// Call the async function
+printHelloAsync();
+
+// making a promise class of your own that functions exactly like the actual promise class
+
+class PromiseCopy {
+    constructor(callback) {
+        callback(this.resolve, this.reject);
+    }
+
+    resolve(pass) {
+        this.pass = pass;
+    }
+
+    reject(err) {
+        this.err = err;
+    }
+
+    then(fn) {
+        return fn(this.pass);
+    }
+
+    catch(fn) {
+        return fn(this.err);
+    }
+};
+
+const p = new PromiseCopy((resolve, reject) => {
+    resolve(100);
+});
+
+p.then(function(x){
+    console.log(x);
+})
 
 
+// real syntax of promises
 
-// Program-6 Write a program that prints all the male people's first name given a complex object.
+function MyOwnPromisifiedTimeout() {
+    return new Promise(function (OnDone, OnError /* Can pass as many functions here as you want */) {
+        OnDone();
+        OnError();
+    });
+}
+
+MyOwnPromisifiedTimeout.then(function () {
+
+})
+    .catch(function () {
+
+    })
 
 
+// a simple code to understand how control flows through a promisified javascript code
+console.log("Printing 1");
+function SampleAsyncFunction() {
+    console.log("Printing 3")
+    return new Promise(function (resolve) {
+        console.log("Printing 4");
+        setTimeout(function () {
+            console.log("Printing 5");
+            resolve();
+        }, 1000);
+    })
+}
 
-// Program-7 Write a program that reverses all the elements of an array
-
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-const personArray = ["Sarthak", "Harkirat", "Sanjay"];
-
-const arraySize = personArray.length;
-
-console.log(personArray[0] + " is best friend of " + personArray[1]);
-
-personArray.push("Abhay");
-personArray.push("Saurabh");
-
-console.log(personArray[3]);
-console.log(arraySize);
-
-const UpdatedArraySize = personArray.length;
-
-console.log(UpdatedArraySize);
-// As updating the previous arraySize will throw error but adding removing changing values of const array doesn't give error
-
+console.log("Printing 2");
+SampleAsyncFunction().then(function () {
+    console.log("Printing 6");
+});
