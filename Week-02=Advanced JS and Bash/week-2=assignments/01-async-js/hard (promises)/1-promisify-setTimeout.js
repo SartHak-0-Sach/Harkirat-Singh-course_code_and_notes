e9fa942ebc914de8d7b23e07f4b529efbf3d6f69
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-09-12 23:32:11 +0530

    Update 1-promisify-setTimeout.js

--- Week-02=Advanced JS and Bash/week-2=assignments/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ Week-02=Advanced JS and Bash/week-2=assignments/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -2,7 +2,20 @@
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
 
-function wait(n) {
+function wait(n)
+{
+    return new Promise((resolve) =>
+    {
+        setTimeout(() =>
+        {
+            resolve(`Resolved after ${n} seconds`);
+        }, n * 1000);
+    });
 }
+
+wait(3).then((message) =>
+{
+    console.log(message); // This will print "Resolved after 3 seconds" after 3 seconds
+});
 
 module.exports = wait;
