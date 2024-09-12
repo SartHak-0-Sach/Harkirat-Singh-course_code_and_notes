b9251f4c35aa7cb6309668d0d91f05648f8ef7d4
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-09-12 23:28:44 +0530

    update to-do list problem solution

--- Week-01=JS fundamentals to advanced/week-1=assignments/hard/todo-list.js
+++ Week-01=JS fundamentals to advanced/week-1=assignments/hard/todo-list.js
@@ -10,8 +10,92 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Todo {
+class Todo
+{
+  constructor()
+  {
+    this.todos = []; // array of strings
+  }
 
+  // Add a todo item
+  add(todo)
+  {
+    this.todos.push(todo);
+  }
+
+  // Remove a todo item by index
+  remove(indexOfTodo)
+  {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length)
+    {
+      this.todos.splice(indexOfTodo, 1);
+    } else
+    {
+      throw new Error('Invalid index');
+    }
+  }
+
+  // Update a todo at a given index
+  update(index, updatedTodo)
+  {
+    if (index >= 0 && index < this.todos.length)
+    {
+      this.todos[index] = updatedTodo;
+    } else
+    {
+      throw new Error('Invalid index');
+    }
+  }
+
+  // Get all todos
+  getAll()
+  {
+    return this.todos;
+  }
+
+  // Get a specific todo by index
+  get(indexOfTodo)
+  {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length)
+    {
+      return this.todos[indexOfTodo];
+    } else
+    {
+      throw new Error('Invalid index');
+    }
+  }
+
+  // Clear all todos
+  clear()
+  {
+    this.todos = [];
+  }
 }
+
+// example immplementation
+const myTodo = new Todo();
+
+// Adding todos
+myTodo.add('Learn JavaScript');
+myTodo.add('Learn Node.js');
+myTodo.add('Write Todo app');
+
+// Get all todos
+console.log(myTodo.getAll());
+
+// Update a todo
+myTodo.update(1, 'Master Node.js');
+console.log(myTodo.getAll());
+
+// Remove a todo
+myTodo.remove(2);
+console.log(myTodo.getAll());  
+
+// Get a specific todo
+console.log(myTodo.get(0));  
+
+// Clear all todos
+myTodo.clear();
+console.log(myTodo.getAll());
 
 module.exports = Todo;
