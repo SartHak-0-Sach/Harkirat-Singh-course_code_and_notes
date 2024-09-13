60dcc6fa0dc2dc1bc181cceb46de6588e96075c6
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-09-13 17:19:13 +0530

    Add solution

--- Week-02=Advanced JS and Bash/week-2=assignments/01-async-js/hard (promises)/2-sleep-completely.js
+++ Week-02=Advanced JS and Bash/week-2=assignments/01-async-js/hard (promises)/2-sleep-completely.js
@@ -4,7 +4,26 @@
  * the function should return a promise just like before
  */
 
-function sleep(milliseconds) {
+function sleep(milliseconds)
+{
+    return new Promise((resolve) =>
+    {
+        const start = Date.now();
+        while (Date.now() - start < milliseconds)
+        {
+            // Busy wait
+        }
+        resolve();
+    });
 }
+
+async function example()
+{
+    console.log('Sleeping...');
+    await sleep(2000); // Sleep for 2000 milliseconds (2 seconds)
+    console.log('Awoke!');
+}
+
+example();
 
 module.exports = sleep;
