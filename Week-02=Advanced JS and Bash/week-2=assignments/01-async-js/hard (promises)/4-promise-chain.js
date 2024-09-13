9394d702b73b26a10b302926d1d8c8ff0dbb9106
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-09-13 17:19:34 +0530

    Add promise chain solution

--- Week-02=Advanced JS and Bash/week-2=assignments/01-async-js/hard (promises)/4-promise-chain.js
+++ Week-02=Advanced JS and Bash/week-2=assignments/01-async-js/hard (promises)/4-promise-chain.js
@@ -5,20 +5,40 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function wait1(t) {
-
+function wait1(t)
+{
+    return new Promise(resolve => setTimeout(resolve, t * 1000));
 }
 
-function wait2(t) {
-
+function wait2(t)
+{
+    return new Promise(resolve => setTimeout(resolve, t * 1000));
 }
 
-function wait3(t) {
-
+function wait3(t)
+{
+    return new Promise(resolve => setTimeout(resolve, t * 1000));
 }
 
-function calculateTime(t1, t2, t3) {
+function calculateTime(t1, t2, t3)
+{
+    const start = Date.now();
 
+    // Chain promises sequentially
+    return wait1(t1)
+        .then(() => wait2(t2))
+        .then(() => wait3(t3))
+        .then(() =>
+        {
+            const end = Date.now();
+            return end - start; // Return the time in milliseconds
+        });
 }
+
+// Example: t1 = 1s, t2 = 2s, t3 = 3s
+calculateTime(1, 2, 3).then((elapsedTime) =>
+{
+    console.log(`Total time (sequential): ${elapsedTime} milliseconds`);
+});
 
 module.exports = calculateTime;
