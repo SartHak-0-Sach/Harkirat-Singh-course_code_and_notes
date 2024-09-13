63f9ed90759f5740a75af2a05998ff9a8cae889d
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-09-13 17:21:27 +0530

    Summarize notes for better understanding

--- Week-03=Introduction to backend and middlewares/lec-1=Zod, middlewares and global catches/code.js
+++ Week-03=Introduction to backend and middlewares/lec-1=Zod, middlewares and global catches/code.js
@@ -1,59 +1,59 @@
-// const express = require('express') // ugly way to do auth because it has lots of bugs and issues in it
-// const app = express()
-// const port = 3000
+const express = require('express') // ugly way to do auth because it has lots of bugs and issues in it
+const app = express()
+const port = 3000
 
 const req = require("express/lib/request");
 
-// app.get('/health-checkup', (req, res) => {
-//     const kidneyId = req.query.kidneyId;
-//     const username = req.headers.username;
-//     const password = req.headers.password;
+app.get('/health-checkup', (req, res) => {
+    const kidneyId = req.query.kidneyId;
+    const username = req.headers.username;
+    const password = req.headers.password;
 
-//     if (username != "sarthak" || password != "123456") { // username and password check
-//         res.status(403).json({
-//             msg: "User doesn't exist"
-//         });
-//         return;
-//     }
+    if (username != "sarthak" || password != "123456") { // username and password check
+        res.status(403).json({
+            msg: "User doesn't exist"
+        });
+        return;
+    }
 
-//     if (kidneyId != 1 && kidneyId != 2) { // input validation
-//         res.status(411).json({
-//             msg: "wrong inputs"
-//         });
-//         return;
-//     }
+    if (kidneyId != 1 && kidneyId != 2) { // input validation
+        res.status(411).json({
+            msg: "wrong inputs"
+        });
+        return;
+    }
 
-//     // do something with the kidneys here
+    // do something with the kidneys here
 
-//     res.send("Your kidneys are healthy");
-// });
+    res.send("Your kidneys are healthy");
+});
 
-// app.put("/replace-kidney", function(req, res){
-//     // do health checks here
-//     const kidneyId = req.query.kidneyId;
-//     const username = req.headers.username;
-//     const password = req.headers.password;
+app.put("/replace-kidney", function(req, res){
+    // do health checks here
+    const kidneyId = req.query.kidneyId;
+    const username = req.headers.username;
+    const password = req.headers.password;
 
-//     if (username != "sarthak" || password != "123456") { // username and password check
-//         res.status(403).json({
-//             msg: "User doesn't exist"
-//         });
-//         return;
-//     }
+    if (username != "sarthak" || password != "123456") { // username and password check
+        res.status(403).json({
+            msg: "User doesn't exist"
+        });
+        return;
+    }
 
-//     if (kidneyId != 1 && kidneyId != 2) { // input validation
-//         res.status(411).json({
-//             msg: "wrong inputs"
-//         });
-//         return;
-//     }
+    if (kidneyId != 1 && kidneyId != 2) { // input validation
+        res.status(411).json({
+            msg: "wrong inputs"
+        });
+        return;
+    }
 
-//     // do something with the kidneys here
+    // do something with the kidneys here
 
-//     res.send("Your kidneys are healthy");
-// })
+    res.send("Your kidneys are healthy");
+})
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // This above mentioned code violates the dry principle
 
@@ -61,68 +61,67 @@ const req = require("express/lib/request");
 
 // Slightly better solution is to use wrapper functions
 
-// const express = require('express')
+const express = require('express')
 // const app = express()
 // const port = 3000
 
-// function usernameValidator(username, password) {
-//     if (username != "sarthak" || password != "123456") // username and password check
-//         return false;
-//     return true;
-// }
+function usernameValidator(username, password) {
+    if (username != "sarthak" || password != "123456") // username and password check
+        return false;
+    return true;
+}
 
-// function inputValidator(kidneyId) {
-//     if (kidneyId != 1 && kidneyId != 2) // input validation
-//         return false;
-//     return true;
-// }
+function inputValidator(kidneyId) {
+    if (kidneyId != 1 && kidneyId != 2) // input validation
+        return false;
+    return true;
+}
 
-// app.get('/health-checkup', (req, res) => {
-//     if (!usernameValidator(req.headers.username, req.headers.password)) {
-//         res.status(403).json({
-//             msg: "User doesn't exist"
-//         });
-//     }
+app.get('/health-checkup', (req, res) => {
+    if (!usernameValidator(req.headers.username, req.headers.password)) {
+        res.status(403).json({
+            msg: "User doesn't exist"
+        });
+    }
 
-//     if (!inputValidator(req.query.kidneyId)) {
-//         res.status(411).json({
-//             msg: "wrong inputs"
-//         });
-//     }
+    if (!inputValidator(req.query.kidneyId)) {
+        res.status(411).json({
+            msg: "wrong inputs"
+        });
+    }
 
-//     // do something with the kidneys here
+    // do something with the kidneys here
 
-//     res.send("Your kidneys are healthy");
-// });
+    res.send("Your kidneys are healthy");
+});
 
-// app.put("/replace-kidney", function (req, res) {
-//     // do health checks here
-//     if (!usernameValidator(req.headers.username, req.headers.password)) {
-//         res.status(403).json({
-//             msg: "User doesn't exist"
-//         });
-//     }
+app.put("/replace-kidney", function (req, res) {
+    // do health checks here
+    if (!usernameValidator(req.headers.username, req.headers.password)) {
+        res.status(403).json({
+            msg: "User doesn't exist"
+        });
+    }
 
-//     if (!inputValidator(req.query.kidneyId)) {
-//         res.status(411).json({
-//             msg: "wrong inputs"
-//         });
-//     }
+    if (!inputValidator(req.query.kidneyId)) {
+        res.status(411).json({
+            msg: "wrong inputs"
+        });
+    }
 
-//     // do something with the kidneys here
+    // do something with the kidneys here
 
-//     res.send("Your kidneys are healthy");
-// })
+    res.send("Your kidneys are healthy");
+})
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // But the issue is there is still a lot of code being repeated here
 
 
-/*
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
 // Rate limiter function
 function rateLimiter(req, res, next) {
@@ -196,7 +195,6 @@ app.get("/get-request2", function(req, res) {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); // old code
-*/
 
 // new app.use code
 
@@ -206,127 +204,127 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`)); /
 
 // let requests = 0;
 
-// function calculaterequests(req, res, next) {
-//     requests++;
-//     console.log("requests are: " + requests);
-//     next(); // Call next to proceed to the next middleware or route handler
-// }
+function calculaterequests(req, res, next) {
+    requests++;
+    console.log("requests are: " + requests);
+    next(); // Call next to proceed to the next middleware or route handler
+}
 
-// // Apply calculaterequests middleware globally
-// app.use(calculaterequests);
+// Apply calculaterequests middleware globally
+app.use(calculaterequests);
 
-// // Define your route handlers
-// app.get("/health-checkup", function (req, res) {
-//     // Do something with the kidney here
-//     res.send("Your heart is healthy");
-// });
+// Define your route handlers
+app.get("/health-checkup", function (req, res) {
+    // Do something with the kidney here
+    res.send("Your heart is healthy");
+});
 
-// app.get("/kidney-check/:kidneyId", function (req, res) {
-//     // Do something with the kidney here
-//     res.send("Your kidney is functioning properly");
-// });
+app.get("/kidney-check/:kidneyId", function (req, res) {
+    // Do something with the kidney here
+    res.send("Your kidney is functioning properly");
+});
 
-// app.get("/heart-check", function (req, res) {
-//     // Do something with the heart here
-//     res.send("Your heart is healthy");
-// });
+app.get("/heart-check", function (req, res) {
+    // Do something with the heart here
+    res.send("Your heart is healthy");
+});
 
-// // Route to increment request count
-// app.get("/add-request", function(req, res) {
-//     res.send("request counted");
-// });
+// Route to increment request count
+app.get("/add-request", function(req, res) {
+    res.send("request counted");
+});
 
-// // Route to get request count
-// app.get("/get-request", function(req, res) {
-//     res.send("requests are: " + requests);
-// });
+// Route to get request count
+app.get("/get-request", function(req, res) {
+    res.send("requests are: " + requests);
+});
 
-// // Second route to get request count
-// app.get("/get-request2", function(req, res) {
-//     res.send("requests are: " + requests);
-// });
+// Second route to get request count
+app.get("/get-request2", function(req, res) {
+    res.send("requests are: " + requests);
+});
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// const express = require('express')
+const express = require('express')
 
 // const app = express()
 // const port = 3000
 
-// // can also do async functions like database calls should be made async
-// async function middleware(req, res, next) {
-//     await fetch();
-//     next();
-// }
+// can also do async functions like database calls should be made async
+async function middleware(req, res, next) {
+    await fetch();
+    next();
+}
 
-// function middleware2(req, res, next) {
-//     fetch().then({})
-//     next();
-// }
+function middleware2(req, res, next) {
+    fetch().then({})
+    next();
+}
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.post("/health-checkup", function (req, res) {
-//     // kidneys = [1,2] expecting an array like this
-//     const kidneys = req.body.kidneys;
-//     const kidneyLength = kidneys.length;
-//     res.send("you have " + kidneyLength + " kidneys");
-// });
+app.post("/health-checkup", function (req, res) {
+    // kidneys = [1,2] expecting an array like this
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+    res.send("you have " + kidneyLength + " kidneys");
+});
 
-// // global catches- catch any error that occurs globally into this codebase
-// let errorCount = 0;
-// app.use(function (err, req, res, next) {
-//     errorCount++; // if there are too many errors coming through, that means something is wrong with the code and not a fault from users
-//     res.json({
-//         msg: "Some error occurred"
-//     })
-// })
+// global catches- catch any error that occurs globally into this codebase
+let errorCount = 0;
+app.use(function (err, req, res, next) {
+    errorCount++; // if there are too many errors coming through, that means something is wrong with the code and not a fault from users
+    res.json({
+        msg: "Some error occurred"
+    })
+})
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // const express = require('express')
 // const app = express()
 // const z = require('zod');
 // const port = 3000
 
-// // const schema = zod.array(zod.number()); // statement telling what kind of input I am expecting from my user
+// const schema = zod.array(zod.number()); // statement telling what kind of input I am expecting from my user
 
-// const schema = z.object({
-//     email: z.string().email(),
-//     password: z.string().min(8),
-//     country: z.literal("IN").or(z.literal("US")),
-//     kidneys: z.array(z.number()) // we can use z or zod both but for calling z I used const z = require('zod') in the package declaration at the top of the code else I would have done zod if I want to write zod.array instead
-// })
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    country: z.literal("IN").or(z.literal("US")),
+    kidneys: z.array(z.number()) // we can use z or zod both but for calling z I used const z = require('zod') in the package declaration at the top of the code else I would have done zod if I want to write zod.array instead
+})
 
-// app.post("/health-checkup", function (req, res) {
-//     // kidneys = [1,2] expecting an array like this
-//     const kidneys = req.body.kidneys;
-//     const response = schema.safeParse(obj);
-//     // const kidneyLength = kidneys.length;
-//     // res.send("you have " + kidneyLength + " kidneys");
-//     // res.send({
-//     //     response
-//     // })
+app.post("/health-checkup", function (req, res) {
+    // kidneys = [1,2] expecting an array like this
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(obj);
+    // const kidneyLength = kidneys.length;
+    // res.send("you have " + kidneyLength + " kidneys");
+    // res.send({
+    //     response
+    // })
 
-//     if (!response.success) {
-//         res.status(400).json({
-//             msg: "Invalid input"
-//         })
-//         return;
-//     }
+    if (!response.success) {
+        res.status(400).json({
+            msg: "Invalid input"
+        })
+        return;
+    }
 
-//     else {
-//         const kidneyLength = kidneys.length;
-//         res.send("you have " + kidneyLength + " kidneys");
-//         res.send({
-//             response
-//         })
-//         console.log(response);
-//     }
-// });
+    else {
+        const kidneyLength = kidneys.length;
+        res.send("you have " + kidneyLength + " kidneys");
+        res.send({
+            response
+        })
+        console.log(response);
+    }
+});
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // HW quetion
 /*
